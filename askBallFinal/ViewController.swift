9b1b5267c66c3606ee322b24f5880c958478b940
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2022-07-08 14:55:51 +0300

    Initial Commit

---
+++ askBallFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  askBallFinal
+//
+//  Created by Ilya Vasilev on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
